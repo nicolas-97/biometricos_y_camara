2ea491e8b1c4d742e6377a49cee283cc18067d12
Nicolas Devel <nicolassabogal85@gmail.com>
2023-10-05 12:30:22 -0500

    camera

--- lib/views/screens/camera/camera.dart
+++ lib/views/screens/camera/camera.dart
@@ -1,14 +1,36 @@
+import 'package:biometrics_camera/views/screens/camera/camera_vm.dart';
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class Camera extends StatelessWidget {
+class Camera extends StatefulWidget {
   const Camera({super.key});
 
   @override
+  State<Camera> createState() => _CameraState();
+}
+
+class _CameraState extends State<Camera> {
+
+@override
+  @override
   Widget build(BuildContext context) {
+    final viewModel = Provider.of<CameraViewModel>(context);
     return Scaffold(
       appBar: AppBar(
+        title: Text('Camara'),
       ),
-      body: Center(),
+      body: viewModel.getIsCameraReady()
+        ? CameraPreview(viewModel.getCameraController())
+        : const Center(child: CircularProgressIndicator(),),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () async {
+            if(viewModel.getIsCameraReady()){
+              final XFile photo = await viewModel.getCameraController().takePicture();
+            }
+          },
+          child: const Icon(Icons.camera),
+        ),
     );
   }
 }
--- lib/views/screens/camera/camera_vm.dart
+++ lib/views/screens/camera/camera_vm.dart
@@ -1,5 +1,26 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 
 class CameraViewModel extends ChangeNotifier{
 
+  late CameraController _cameraController;
+  late List<CameraDescription> _cameras;
+  bool _isCameraReady = false;
+
+  CameraViewModel(){
+    initializeCamera(); 
+  }
+
+  Future<void> initializeCamera() async {
+    _cameras = await availableCameras();
+    _cameraController = CameraController(_cameras[1], ResolutionPreset.high);
+    await _cameraController.initialize();
+    _isCameraReady = true;
+    notifyListeners();
+  }
+
+  CameraController getCameraController() => _cameraController;
+  bool getIsCameraReady() => _isCameraReady;
+
+  
 }
